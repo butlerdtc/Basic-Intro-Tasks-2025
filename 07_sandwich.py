17fa68da12678d1ee452136d4db8c5546ebb0e3b
butlerdtc <butlerr2@middleton.school.nz>
2025-02-13 20:34:10 +1300

    Create 07_sandwich.py

---
+++ 07_sandwich.py
@@ -0,0 +1,8 @@
+options = {
+    "Bread": {"Wholemeal": 1.00, "White": 0.80, "Cheesy White": 1.20,
+              "Gluten Free": 1.40},
+    "Meat": {"Chicken": 2.69, "Beef": 3.00, "Salami": 4.00, "Vegan Slice":
+        3.30},
+    "Garnish": {"Onion": 1.69, "Tomato": 1.00, "Lettuce": 2.00, "Cheese": 2.50}
+}
+
